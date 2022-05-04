bec1eaf230606a8ad7bc01498503f3e35d639a06
oscar-olle <104491176+oscar-olle@users.noreply.github.com>
2022-05-04 09:47:01 +0200

    Initiala filer för MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,14 @@
+let goal = 0
+let score = 0
+basic.showNumber(score)
+basic.pause(100)
+basic.forever(function () {
+    goal = pins.digitalReadPin(DigitalPin.P0)
+    if (goal == 1) {
+        score = score + 1
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.showNumber(score)
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
+})
